7713a50b968e87dde32ca6876672ce950ad09912
Brooke Stanislawski <brooke.stanislawski@utah.edu>
2018-02-22 14:28:43 -0700

    progress

--- HW6/HW6_scripts.py
+++ HW6/HW6_scripts.py
@@ -33,20 +33,53 @@ plt.rcParams['figure.figsize'] = (10, 6)
 ## Your code goes here
 
 # download the fist ten pages
+for i in np.arange(1,11):
+    
 url = "https://github.com/search?o=desc&p=1&q=stars%3A%3E1&s=stars&type=Repositories"
 
-# with urllib.request.urlopen(url) as response:
-#     html = response.read()
-#     html = html.decode('utf-8')
 
-#A simple solution is to just use ‘response.text’ to get html returned by requests.get(url) directly.
-    
-requests.get(url)
-html = response.text()
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    html = html.decode('utf-8')
 
-# save the file
+#A simple solution is to just use ‘response.text’ to get html returned by requests.get(url) directly. This doesn't work on Windows (only Mac and Linux)
+# html = requests.get(url)
+# html = html.text
+
+# # save the file
 with open('git_repositories.html', 'w') as new_file:
-    new_file.write(html)
+    new_file.write(str(html.replace('\n','').encode('utf-8')))
 
-# here it's already a local operation
 git_soup = BeautifulSoup(html, 'html.parser')
+git_soup = BeautifulSoup(html, 'html.parser')
+
+
+name_list = []
+url_list = []
+cont_list = []
+
+repos = git_soup.find_all("div",class_="col-8 pr-3")
+url = "https://github.com"
+
+i = 0
+#for i in range(10):
+    name_list.append(repos[i].find("a",class_="v-align-middle").text)
+    url_tail = repos[i].find("a").get("href")
+    repo_url = url + url_tail
+    url_list.append(repo_url)
+    with urllib.request.urlopen(repo_url) as response:
+        repo_html = response.read()
+        repo_html = repo_html.decode('utf-8')
+    with open('repo.html', 'w') as new_file:
+        new_file.write(str(repo_html.replace('\n','').encode('utf-8')))
+    repo_soup = BeautifulSoup(repo_html, 'html.parser')
+    cont_list.append(repo_soup.find("a", href= lambda x : x and "contributors" in x).text)
+    
+    
+    
+    
+    #num_cont_list.append(repos[i].find("a").get("href"))
+
+print('names:',name_list,'\n')
+print('URLs:',url_list,'\n')
+print('cont_list:',cont_list,'\n')
