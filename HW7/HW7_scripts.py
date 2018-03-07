8965c6e83d9924a018188451a6bda682df6182d0
Brooke Stanislawski <brooke.stanislawski@utah.edu>
2018-03-07 12:38:38 -0700

    progress

--- HW7/HW7_scripts.py
+++ HW7/HW7_scripts.py
@@ -51,10 +51,10 @@ plt.show()
 # your solution goes here
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.8)
 
-model = svm.SVC(kernel='rbf',C=100)
-model.fit(X_train, y_train)
+dig_svm = svm.SVC(kernel='rbf',C=100)  # change this one 6, 5, 4, 3, 2
+dig_svm.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+y_pred = dig_svm.predict(X_test)
 print('Confusion Matrix')
 print(metrics.confusion_matrix(y_true = y_test, y_pred = y_pred))
 print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pred = y_pred))
@@ -68,13 +68,13 @@ for ii in y_test:
     #  ??????????????????????
     
 #%% Task 1.1.5 
-model.get_params()
+dig_svm.get_params()
 
 Cs = np.linspace(1,500,100)
 Accuracies = np.zeros(Cs.shape[0])
 for i,C in enumerate(Cs): # what is enumerate?
-    svm_model = svm.SVC(kernel='rbf', C = C)
-    scores = cross_val_score(estimator = svm_model, X = X, y = y, cv=5, scoring='accuracy')     # cv = 5 ??
+    dig_svm = svm.SVC(kernel='rbf', C = C)
+    scores = cross_val_score(estimator = dig_svm, X = X, y = y, cv=5, scoring='accuracy')     # cv = 5 ??
     Accuracies[i]  = scores.mean()
     
 #%%
@@ -83,7 +83,7 @@ plt.plot(Cs,Accuracies)
 plt.title('Evaluating accuracy of C')
 plt.xlabel('C value')
 plt.ylabel('Accuracy')
-plt.xlim(0,10)
+plt.xlim(0,500) # change this to 0, 10
 plt.show()
 
 # choose any C greater than or equal to 6?
@@ -93,12 +93,12 @@ plt.show()
 #%% Task 1.1.6
 
 X = digits.data
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.8)
 
-model = svm.SVC(kernel='rbf',C=100)
-model.fit(X_train, y_train)
+rawdig_svm = svm.SVC(kernel='rbf',C=100)
+rawdig_svm.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+y_pred = rawdig_svm.predict(X_test)
 print('Confusion Matrix')
 print(metrics.confusion_matrix(y_true = y_test, y_pred = y_pred))
 print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pred = y_pred))
@@ -106,11 +106,28 @@ print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pred = y_pred))
 # Accuracy of 0.1182, which is terrible
 # Almost all data was classified as '0' (first column)
 
+# change test_size from 0.8 to 0.5
+# Accuracy =  0.290322580645
+
+# change test_Size from 0.5 to 0.1
+# Accuracy =  0.561111111111
+
 # is this wrong?  training the model on so much data that there is barelly enough to test it with
+
+# %% Your interpretation
+
+#Using the test dataset, the model's accuracy is 95.4%, meaning that 95.4% points were classified correctly. As seen in the confusion matrix, the most common mistake made by the model is found in the cell containing the value '12,' meaning that the model incorrectly classified 12 digits as '7' when they were actually a '4.' ????
+#
+#In Task 1.1.5, we evaluate accuracies of the SVM model and found that any C greater than or equal to 6 results in a constant maximum accuracy of 95.4%.
+#
+#When using a test_size of 0.8, I got an accuracy of 0.1182, which is terrible. Almost all data was classified as '0'. I then decreased the test_size from 0.8 to 0.5 to 0.1 to get an accuracy of 0.561, but this may be a questionable strategy since there is barely any data to test the algorithm on (10%).
+
+
+
 
 #%% Task 1.2.1
 # set up the model, k-NN classification with k = ?  
-k = 10
+k = 10 # when k = 3, 6, not matching iterative model results?
 X = scale(digits.data)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.8)
 knn_model = KNeighborsClassifier(n_neighbors=k)
@@ -119,7 +136,7 @@ knn_model.fit(X_train, y_train)
 y_pred = knn_model.predict(X_test)
 print('Confusion Matrix')
 print(metrics.confusion_matrix(y_true = y_test, y_pred = y_pred))
-print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pbred = y_pred))
+print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pred = y_pred))
 
 # Accuracy of 0.9124 w/ k = 10
 # Most common mistake: classified 11 data points as Class 2 when really they were class 1
@@ -160,7 +177,20 @@ plt.show()
 # choose k = 6
 # when k = 6, Accuracy =  0.922114047288
 
-#%% Part 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%% Part 2 ###########################################################
 # Task 2.1
 newsdf = pd.read_csv('OnlineNewsPopularity/OnlineNewsPopularity.csv')
 newsdf.columns = newsdf.columns.str.replace(" ","") # get rid of extra spaces in attribute names (headers)
@@ -180,23 +210,30 @@ newsdf.describe() # better
 
 print('Cleaned news dataframe shape: ',newsdf.shape,'\n')
 
-newsdf['shares'].describe()
+print('Shares Statistical info')
+print(newsdf['shares'].describe())
+newsdf['shares'].median()
+
+#median number of shares = 1400
 
 #From Task 2.1 part 3
 # export predictor vars as np array
 X = newsdf.drop(['url','timedelta','shares'],axis=1).as_matrix()
 shares = newsdf['shares'].as_matrix()
-y = [1 if x > 1400 else 0 for x in shares] #binary numpy array, y, which indicates whether or not each article is popular
+y = [1 if x > newsdf['shares'].median() else 0 for x in shares] #binary numpy array, y, which indicates whether or not each article is popular
 
-#%% Task 2.3
+print('Predictor Variable Data Shape: ',X.shape)
+print('Shares array length: ',len(y))
+
+#%% Task 2.3 k-NN ################################################
 
 k = 10
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.8)
-knn_model = KNeighborsClassifier(n_neighbors=k)
-knn_model.fit(X_train, y_train)
+news_knn = KNeighborsClassifier(n_neighbors=k)
+news_knn.fit(X_train, y_train)
 
-y_pred = knn_model.predict(X_test)
+y_pred = news_knn.predict(X_test)
 print('Confusion Matrix')
 print(metrics.confusion_matrix(y_true = y_test, y_pred = y_pred))
 print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pred = y_pred))
@@ -205,13 +242,13 @@ print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pred = y_pred))
 
 #%%
 
-knn_model.get_params()
+news_knn.get_params()
 
 Ks = np.linspace(1,100,100)
 Accuracies = np.zeros(Ks.shape[0])
 for i,k in enumerate(Ks): # what is enumerate?
-    knn_model = KNeighborsClassifier(n_neighbors=int(k))
-    scores = cross_val_score(estimator = knn_model, X = X, y = y, cv=5, scoring='accuracy')     # cv = 5 ??
+    news_knn = KNeighborsClassifier(n_neighbors=int(k))
+    scores = cross_val_score(estimator = news_knn, X = X, y = y, cv=5, scoring='accuracy')     # cv = 5 ??
     Accuracies[i]  = scores.mean()
     
 plt.plot(Ks,Accuracies)
@@ -220,7 +257,7 @@ plt.xlabel('k value')
 plt.ylabel('Accuracy')
 plt.show()
     
-#%%
+#%% 
     
 plt.plot(Ks,Accuracies)
 plt.title('Evaluating accuracy of k-values')
@@ -232,31 +269,38 @@ plt.show()
 # choose k = 37
 # when k = 37, Accuracy =  0.564401702664 - still not that great
 
-#%% Task 2.4
+# %% Your interpretation
+
+#At the start, I arbitrarily chose k = 10, which yielded an accuracy of 0.55, which is not too impressive.  I evaluated the accuracy at 100 k-values between 1 and 100, which illustrated that the k-value of 37 yielded the maximum accuracy of 0.564, which is only  marginally better.
+
+#%% Task 2.4 SVM ################################################
 
 Xsubset = X[:50][:]
 ysubset = y[:50]
 
 X_train, X_test, y_train, y_test = train_test_split(Xsubset, ysubset, random_state=1, test_size=0.8)
 
-svm_model = svm.SVC(kernel='rbf',C=100)
-svm_model.fit(X_train, y_train)
+news_svm = svm.SVC(kernel='rbf',C=100)
+news_svm.fit(X_train, y_train)
 
-y_pred = svm_model.predict(X_test)
+y_pred = news_svm.predict(X_test)
 print('Confusion Matrix')
 print(metrics.confusion_matrix(y_true = y_test, y_pred = y_pred))
 print('Accuracy = ', metrics.accuracy_score(y_true = y_test, y_pred = y_pred))
 
-# when C = 100, Accuracy =  0.56175
+# when C = 100, Accuracy =  0.6
 
 #%%
-svm_model.get_params()
+news_svm.get_params()
+X = Xsubset
+y = ysubset
 
 Cs = np.linspace(1,500,100)
 Accuracies = np.zeros(Cs.shape[0])
 for i,C in enumerate(Cs): # what is enumerate?
-    svm_model = svm.SVC(kernel='rbf', C = C)
-    scores = cross_val_score(estimator = svm_model, X = X, y = y, cv=5, scoring='accuracy')     # cv = 5 ??
+    print(i,C)
+    news_svm = svm.SVC(kernel='rbf', C = C)
+    scores = cross_val_score(estimator = news_svm, X = X, y = y, cv=5, scoring='accuracy')     # cv = 5 ??
     Accuracies[i]  = scores.mean()
     
 #%%
@@ -265,7 +309,31 @@ plt.plot(Cs,Accuracies)
 plt.title('Evaluating accuracy of C')
 plt.xlabel('C value')
 plt.ylabel('Accuracy')
-plt.xlim(0,10)
+#plt.xlim(0,10)
 plt.show()
 
+#%% Task 2.5 Decision Trees ##################################
+
+labels =["Popular", "Unpopular"]
+
+def splitData(features):
+    news_predictors = X # titanic[features].as_matrix()
+    news_labels = y # titanic["Survived"].as_matrix()
+
+    # Split into training and test sets
+    XTrain, XTest, yTrain, yTest = train_test_split(news_predictors, news_labels, random_state=1, test_size=0.8)
+    return XTrain, XTest, yTrain, yTest
+
 #%%
+
+all_features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+XTrain, XTest, yTrain, yTest = splitData(all_features)
+decisionTree = tree.DecisionTreeClassifier()
+decisionTree = decisionTree.fit(XTrain, yTrain)
+
+y_pred_train = decisionTree.predict(XTrain)
+print('Accuracy on training data= ', metrics.accuracy_score(y_true = yTrain, y_pred = y_pred_train))
+
+y_pred = decisionTree.predict(XTest)
+print('Accuracy on test data= ', metrics.accuracy_score(y_true = yTest, y_pred = y_pred))
